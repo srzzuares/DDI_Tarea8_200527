466d29993746412f5150d8d0b25428082bf8e8c1
sr <srzzuares@gmail.com>
2023-06-22 18:10:50 -0600

    Latitude and Longitude

--- app/src/main/java/mx/edu/utxj/ddi/tarea8_200527/presentation/MainActivity.kt
+++ app/src/main/java/mx/edu/utxj/ddi/tarea8_200527/presentation/MainActivity.kt
@@ -6,64 +6,110 @@
 
 package mx.edu.utxj.ddi.tarea8_200527.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import mx.edu.utxj.ddi.tarea8_200527.R
 import mx.edu.utxj.ddi.tarea8_200527.presentation.theme.Tarea8_200527Theme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var latitude: TextView
+    private lateinit var longitude: TextView
+    private lateinit var handler: Handler
+    private lateinit var updateTimeRunnable: Runnable
+    private var mHandler: Handler? = null
+    private var mRunnable: Runnable? = null
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var locationCallback: LocationCallback? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WearApp("Android")
+        setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult == null) {
+                    return
+                }
+                for (location in locationResult.locations) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    val latitudes = findViewById<TextView>(R.id.latitudes)
+                    val longitudes = findViewById<TextView>(R.id.longitudes)
+
+                    latitudes.text = "Latitud: $latitude"
+                    longitudes.text = "Longitud: $longitude"
+                }
+            }
         }
-    }
+
+        mHandler = Handler()
+        mRunnable = Runnable { // Actualiza cada segundo
+        }
 }
 
-@Composable
-fun WearApp(greetingName: String) {
-    Tarea8_200527Theme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-         * version of LazyColumn for wear devices with some added features. For more information,
-         * see d.android.com/wear/compose.
-         */
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
+
+
+
+    override fun onResume() {
+        super.onResume();
+        startLocationUpdates();
+        mRunnable?.let { mHandler?.post(it) }
+    }
+
+    override fun onPause() {
+        super.onPause();
+        stopLocationUpdates();
+        mRunnable?.let { mHandler?.removeCallbacks(it) }
+    }
+
+    private fun startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
         ) {
-            Greeting(greetingName = greetingName)
+            val locationRequest: LocationRequest = LocationRequest.create()
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            locationRequest.setInterval(5000) // Intervalo en milisegundos para obtener actualizaciones de ubicación
+            locationCallback?.let {
+                fusedLocationClient!!.requestLocationUpdates(locationRequest,
+                    it, null)
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
         }
     }
-}
 
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
+    private fun stopLocationUpdates() {
+        locationCallback?.let { fusedLocationClient!!.removeLocationUpdates(it) }
+    }
 
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            }
+        }
+    }
 }
